bb70da7c18dc2d60a3352bea9f6b6856d0e13a3d
Dev Sanghvi <dys27@cornell.edu>
2019-09-03 15:05:43 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,4 @@
+#Basic template for all models - importing Libraries, dataset, training/testing, feature scaling for some
 #importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,3 +23,13 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
+
+#Splitting the dataset into training data and testing data
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.2, random_state = 0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
